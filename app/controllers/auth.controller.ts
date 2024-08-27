e38602f137613b5887f12c525c52291c15988e24
Dwipa Yogiswara <dwipayogi50@gmail.com>
2024-08-27 20:25:58 +0700

    chore: Refactor customer to user in auth controller

--- app/controllers/auth.controller.ts
+++ app/controllers/auth.controller.ts
@@ -24,7 +24,7 @@ export const SignUp = async (req: Request, res: Response) => {
 
   const hashedPassword = await bcrypt.hash(password, 8);
 
-  const customer = await prisma.customer
+  const user = await prisma.user
     .create({
       data: {
         firstname: firstname,
@@ -34,13 +34,13 @@ export const SignUp = async (req: Request, res: Response) => {
         address: address,
       },
     })
-    .then((customer) => {
-      res.json(customer);
+    .then((user) => {
+      res.json(user);
     })
     .catch((error) => {
       res.status(500).send({
         message:
-          error.message || "Some error occurred while creating the Customer.",
+          error.message || "Some error occurred while creating the user.",
       });
     });
 };
@@ -54,18 +54,18 @@ export const SignIn = async (req: Request, res: Response) => {
     });
   }
 
-  const customer = await prisma.customer
+  const user = await prisma.user
     .findUnique({
       where: { email: email },
     })
-    .then(async (customer) => {
-      if (!customer) {
+    .then(async (user) => {
+      if (!user) {
         return res.status(404).send({
-          message: "Customer not found",
+          message: "user not found",
         });
       }
 
-      const passwordIsValid = await bcrypt.compare(password, customer.password);
+      const passwordIsValid = await bcrypt.compare(password, user.password);
 
       if (!passwordIsValid) {
         return res.status(401).send({
@@ -73,19 +73,19 @@ export const SignIn = async (req: Request, res: Response) => {
         });
       }
 
-      const token = jwt.sign({ id: customer.customer_id }, secretKey, {
+      const token = jwt.sign({ id: user.user_id }, secretKey, {
         expiresIn: 86400, // 24 hours
       });
 
       res.send({
-        customer: customer,
+        user: user,
         accessToken: token,
       });
     })
     .catch((error) => {
       res.status(500).send({
         message:
-          error.message || "Some error occurred while signing in the Customer.",
+          error.message || "Some error occurred while signing in the user.",
       });
     });
 };
